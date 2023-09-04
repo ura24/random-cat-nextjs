36d72c4a2133c76617c39d6066b3466ae73db2b4
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-04 22:22:33 +0900

    関数の戻り値に型をつける

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,34 @@ const IndexPage: NextPage = () => {
 
 export default IndexPage;
 
-const fetchImage = async () => {
+type Image = {
+    url: string
+}
+const fetchImage = async (): Promise<Image> => {
     const res = await fetch("https://api.thecatapi.com/v1/images/search");
-    const images = await res.json();
-    console.log(images);
-    return images[0]
+    const images: unknown = await res.json();
+
+    // 配列として表現されているかチェック
+    if (!Array.isArray(images)) {
+        throw new Error("猫の画像が取得できませんでした")
+    }
+    
+    const image: unknown = images[0]
+    // Imageの構造かどうかチェック
+    if (!isImage(image)) {
+        throw new Error("猫の画像が取得できませんでした")
+    }
+
+    return image
 }
 
+// 型ガード関数
+const isImage = (value: unknown): value is Image => {
+    // 値がオブジェクトかチェック
+    if (!value || typeof value !== "object") {
+        return false;
+    }
+    // urlプロパティが存在し、かつ、それが文字列なのかチェック
+    return "url" in value && typeof value.url == "string";
+}
 fetchImage()
