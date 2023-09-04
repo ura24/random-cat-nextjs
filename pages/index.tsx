8033ab459e1375c1443c4304835e8defed74f6a3
Yuto Urashima <yuto.urashima24@gmail.com>
2023-09-04 21:44:36 +0900

    画像を取得する関数を実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,3 +5,12 @@ const IndexPage: NextPage = () => {
 }
 
 export default IndexPage;
+
+const fetchImage = async () => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0]
+}
+
+fetchImage()
